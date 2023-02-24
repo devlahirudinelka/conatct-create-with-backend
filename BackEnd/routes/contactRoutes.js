16b59e0bb3b0319e2e82896e0c80f049bf6ead7b
Lahiru D K Yapa <lahirudinelka0611@gmail.com>
2023-02-24 22:46:54 +0530

    first commit

---
+++ BackEnd/routes/contactRoutes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+router.route("/").get((req, res) => {
+    res.status(200).json(
+        { massage: "Get All Contacts" }
+    );
+});
+
+router.route("/:id").get((req, res) => {
+    res.status(200).json(
+        { massage: `Get Contact for ${req.params.id}` }
+    );
+});
+
+router.route("/").post((req, res) => {
+    res.status(200).json(
+        { massage: "Create Contact" }
+    );
+});
+
+router.route("/:id").put((req, res) => {
+    res.status(200).json(
+        { massage: `Update Contact for ${req.params.id}` }
+    );
+});
+
+router.route("/:id").delete((req, res) => {
+    res.status(200).json(
+        { massage: `Delete Contact for ${req.params.id}` }
+    );
+});
+
+module.exports = router;
