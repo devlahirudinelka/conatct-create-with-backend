4ffb9c18a1629b8e85200940018add162cfa9fe4
Lahiru D K Yapa <lahirudinelka0611@gmail.com>
2023-02-25 14:20:10 +0530

    Add Connect databse

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/dbConntion');
 const errorHandler = require('./middleware/errorHandler');
 const dotenv = require("dotenv").config();
+
+connectDB();
+
 const app = express();
 const port = process.env.PORT || 5000;
 // add middleware to display data
 app.use(express.json());
 // 
 // routes
-app.use("/api/contacts", require("./routes/contactRoutes")); 
+app.use("/api/contacts", require("./routes/contactRoutes"));
 //
 // middleware
 app.use(errorHandler);
